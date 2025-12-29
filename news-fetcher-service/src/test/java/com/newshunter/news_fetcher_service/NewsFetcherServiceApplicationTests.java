bdf04b27aa9a7fcfdd1f50de367028aeb046994f
Mahmoud-Elbrer <111395110+Mahmoud-Elbrer@users.noreply.github.com>
2025-12-29 17:31:55 +0400

    init project rss fetcher services

---
+++ news-fetcher-service/src/test/java/com/newshunter/news_fetcher_service/NewsFetcherServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newshunter.news_fetcher_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsFetcherServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

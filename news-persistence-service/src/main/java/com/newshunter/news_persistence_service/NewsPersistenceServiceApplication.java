76ea7381a8033126f0834c93354e7d17d69f78ac
Mahmoud-Elbrer <111395110+Mahmoud-Elbrer@users.noreply.github.com>
2025-12-30 02:14:08 +0400

    v1 RSS fetcher for news websites

---
+++ news-persistence-service/src/main/java/com/newshunter/news_persistence_service/NewsPersistenceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.newshunter.news_persistence_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsPersistenceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsPersistenceServiceApplication.class, args);
+	}
+
+}
---
+++ news-persistence-service/src/test/java/com/newshunter/news_persistence_service/NewsPersistenceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newshunter.news_persistence_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsPersistenceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

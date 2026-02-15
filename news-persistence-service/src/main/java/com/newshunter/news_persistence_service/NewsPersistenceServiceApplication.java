2f3944b73ac5c22622604eefb61cf6a8f71c23a8
Mahmoud-Elbrer <111395110+Mahmoud-Elbrer@users.noreply.github.com>
2026-02-15 19:13:10 +0400

    inhance batching inserts in news_persistence_service

--- news-persistence-service/src/main/java/com/newshunter/news_persistence_service/NewsPersistenceServiceApplication.java
+++ news-persistence-service/src/main/java/com/newshunter/news_persistence_service/NewsPersistenceServiceApplication.java
@@ -2,12 +2,12 @@ package com.newshunter.news_persistence_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class NewsPersistenceServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(NewsPersistenceServiceApplication.class, args);
 	}
-
 }
--- news-persistence-service/src/main/java/com/newshunter/news_persistence_service/kafka/NewsConsumer.java
+++ news-persistence-service/src/main/java/com/newshunter/news_persistence_service/kafka/NewsConsumer.java
@@ -6,6 +6,7 @@ import com.newshunter.news_persistence_service.repository.NewsRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -25,7 +26,7 @@ public class NewsConsumer {
     private final List<com.newshunter.news_persistence_service.entity.News> batchBuffer = Collections.synchronizedList(new ArrayList<>());
 
     // Define batch size (you can tune this based on load)
-    private static final int BATCH_SIZE = 100;
+    private static final int BATCH_SIZE = 40;
 
     public NewsConsumer(NewsRepository newsRepository) {
         this.newsRepository = newsRepository;
@@ -39,24 +40,52 @@ public class NewsConsumer {
         LOGGER.info("Message received in News Persistence Service :  -> " + news);
 
         // Map fetcher News to persistence News
-        com.newshunter.news_persistence_service.entity.News persistenceNews = NewsMapper.mapToPersistenceNews(news);
+        // com.newshunter.news_persistence_service.entity.News persistenceNews
+        var persistenceNews = NewsMapper.mapToPersistenceNews(news);
 
         // Add entity to batch buffer instead of saving immediately
         batchBuffer.add(persistenceNews);
 
         // When buffer reaches defined batch size → insert all at once
         if (batchBuffer.size() >= BATCH_SIZE) {
-
-            // Clear buffer after successful insert
-            newsRepository.saveAll(batchBuffer);
-
-            // Clear buffer after successful insert
-            batchBuffer.clear();
-
+            flushBatch();
         }
 
-        // todo : if size not reached
-        // make Scheduled
+    }
+
+    // Flush by time (every 20 seconds)
+    @Scheduled(fixedDelay = 20000)
+    public void scheduledFlush() {
+        if (!batchBuffer.isEmpty()) {
+            flushBatch();
+        }
+    }
+
+
+    private void flushBatch() {
+
+        synchronized (batchBuffer) {
+
+            if (batchBuffer.isEmpty()) {
+                return;
+            }
+
+            try {
+                // save batch to db
+                newsRepository.saveAll(batchBuffer);
+
+                LOGGER.info("Inserted batch of {} records", batchBuffer.size());
+
+                // Clear buffer after successful insert
+                batchBuffer.clear();
+
+            } catch (Exception e) {
+
+                LOGGER.error("Batch insert failed", e);
+
+                // Optional: retry logic
+            }
+        }
     }
 
 
